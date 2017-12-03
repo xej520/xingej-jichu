44b971dc94b5d66115a4fd70d240ccbcd1c45afb
邢尔军 <xingej@163.com>
2017-12-03 09:32:57 +0800

    JSON对象创建方式二：使用HashMap结构来实现

--- Java-jichu/src/test/java/com/xingej/jichu/json/JSONTest.java
+++ Java-jichu/src/test/java/com/xingej/jichu/json/JSONTest.java
@@ -1,6 +1,8 @@
 package com.xingej.jichu.json;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -23,6 +25,24 @@ public class JSONTest {
         wangxiaoer.put("car", null);
 
         System.out.println("====>:\t" + wangxiaoer.toString());
+
+    }
+
+    // 使用HashMap结构，来构造JSONObject
+    @Test
+    public void testByMap() {
+
+        Map<String, Object> wangxiaoer = new HashMap<>();
+
+        wangxiaoer.put("name", "王小二");
+        wangxiaoer.put("age", 29);
+        wangxiaoer.put("birthday", new Date());
+        wangxiaoer.put("school", "蓝翔");
+        wangxiaoer.put("major", new String[] { "理发", "挖掘机" });
+        wangxiaoer.put("has_girlfriend", false);
+        wangxiaoer.put("car", null);
+
+        System.out.println("====>:\t" + new JSONObject(wangxiaoer).toString());
 
     }
 
