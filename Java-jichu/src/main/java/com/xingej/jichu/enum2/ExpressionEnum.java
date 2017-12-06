75e74bae83d4e420d3ac56cb7a79aede15712acb
邢尔军 <xingej@163.com>
2017-12-07 06:17:23 +0800

    枚举：使用场景：定义常量；传入参数，如何获取参数值getXXX()方法，如何获取对象的名称name()方法

---
+++ Java-jichu/src/main/java/com/xingej/jichu/enum2/ExpressionEnum.java
@@ -0,0 +1,34 @@
+package com.xingej.jichu.enum2;
+
+/**
+ * 枚举作用？或者说，使用场景：定义常量值
+ * 
+ * 不能提供setXXX方法
+ * 
+ * @author erjun 2017年12月7日 上午5:49:13
+ */
+public enum ExpressionEnum {
+    // dot，separator。。。。。就是当前枚举类ExpressionEnum的实例对象名称
+    // ".","/" 是传入的参数
+    dot("."), separator("/"), dollar("$"), openBracket("["), closeBracket("]"), equal("="), comma(",");
+
+    private String expr;
+
+    private ExpressionEnum(String expr) {
+        this.expr = expr;
+    }
+
+    // 通过这个方法，可以将".","/"取出来
+    public String getExpr() {
+        return this.expr;
+    }
+
+    // 简单测试
+    public static void main(String[] args) {
+        System.out.println(ExpressionEnum.dollar.getExpr());// 获得的是，传入的参数值
+        System.out.println(ExpressionEnum.dollar.name()); // name()得到的是，对象名称
+        System.out.println(ExpressionEnum.dollar.values().length); // 对象名称的长度
+
+    }
+
+}
---
+++ Java-jichu/src/main/java/com/xingej/jichu/enum2/GenConfEnum.java
@@ -0,0 +1,10 @@
+package com.xingej.jichu.enum2;
+
+/**
+ * 只定义元素
+ * 
+ * @author erjun 2017年12月7日 上午5:52:43
+ */
+public enum GenConfEnum {
+    GenConf, NeedGens, NeedGen, Params, Param, Themes, Theme, Constants, Constant, id, provider, themeId
+}
