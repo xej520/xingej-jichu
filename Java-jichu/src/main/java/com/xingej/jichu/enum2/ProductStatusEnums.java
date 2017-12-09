818e9cab08d8890195f6c83b648f60669798712c
邢尔军 <xingej@163.com>
2017-12-10 06:00:40 +0800

    增加枚举类，商品状态枚举类，上架，下架，两个属性来表示一个是code, 一个message

---
+++ Java-jichu/src/main/java/com/xingej/jichu/enum2/ProductStatusEnums.java
@@ -0,0 +1,40 @@
+package com.xingej.jichu.enum2;
+
+/**
+ * 枚举 ：产品
+ * 
+ * 枚举类:构造方法私有化，提供外界访问的接口API
+ * 
+ * @author erjun 2017年12月10日 上午5:34:18
+ */
+public enum ProductStatusEnums {
+
+    UP(0, "上架"), DOWN(1, "下架");
+
+    // 注意，这里有两个属性，一个code， 一个message
+    private Integer code;
+
+    private String message;
+
+    // 注意，枚举类的构造方法，也是
+    // 私有的哦
+    private ProductStatusEnums(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    // 枚举类里，
+    // 提供外界访问的
+    // 私有方法
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---->:\t" + ProductStatusEnums.UP.getMessage());
+    }
+}
