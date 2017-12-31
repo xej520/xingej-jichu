1e933a7ae6d98d2f36a96b15f108300f07c29e3f
邢尔军 <xingej@163.com>
2017-12-31 16:31:06 +0800

    初始化工程，  【类加载器】

---
+++ xingej-classloader/src/main/java/com/dongnao/classloader/ClassLoaderTest.java
@@ -0,0 +1,9 @@
+package com.dongnao.classloader;
+
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        // 我们现在要将这个文件加载到JVM(虚拟机)，而且
+        // 我们要得到加载这个文件的类加载的名字
+
+    }
+}
