f693abfeaddca5e38f43e9b4a66f003785f5caef
邢尔军 <xingej@163.com>
2017-12-31 16:40:35 +0800

    【类加载器】：获取当前类的类加载器名字

--- xingej-classloader/src/main/java/com/dongnao/classloader/ClassLoaderTest.java
+++ xingej-classloader/src/main/java/com/dongnao/classloader/ClassLoaderTest.java
@@ -4,6 +4,9 @@ public class ClassLoaderTest {
     public static void main(String[] args) {
         // 我们现在要将这个文件加载到JVM(虚拟机)，而且
         // 我们要得到加载这个文件的类加载的名字
+        String className = ClassLoaderTest.class.getClassLoader().getClass().getName();
+
+        System.out.println("----类加载的名字----:\t" + className);
 
     }
 }
