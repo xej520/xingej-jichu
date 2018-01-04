d3ebbfbbd4515834cf7f9b79855e86e0e5aa47c1
邢尔军 <xingej@163.com>
2018-01-04 16:27:29 +0800

    获取，运行时期，产生的代理对象，并保存到指定的目录，从而分析JDK动态代理的本质

--- xingej-proxey/src/test/java/com/xingej/proxy/ProxyHandlerTest.java
+++ xingej-proxey/src/test/java/com/xingej/proxy/ProxyHandlerTest.java
@@ -1,10 +1,13 @@
 package com.xingej.proxy;
 
+import java.io.FileOutputStream;
 import java.lang.reflect.Proxy;
 
 import org.junit.Test;
 
 import com.xingej.common.utils.PropertiesUtil;
+
+import sun.misc.ProxyGenerator;   
 
 
 public class ProxyHandlerTest {
@@ -19,7 +22,8 @@ public class ProxyHandlerTest {
         //从而使得，代理对象，也再不断变化
         // 也就是说，前两个参数是固定的，
         // 第3个参数可以动态变化，如通过配置文件读取
-        //
+        //java程序，分为：编码器，编译器，运行器
+        //其中，下面的语句，动态代理，是在运行器的，因此，我们根本看不到的
         Person proxy = (Person) Proxy.newProxyInstance(Person.class.getClassLoader(), new Class[]{Person.class}, 
                 new ProxyHandler(person));
         
@@ -54,7 +58,48 @@ public class ProxyHandlerTest {
         proxy.eat();
     }
     
+    //获取，运行时期，产生的代理对象，并保存到指定的目录
+    @Test
+    public void test3() throws Exception{
+        //创建一个被代理对象
+        Person person = new ZhangSan();
+        
+        //java程序，分为：编码器，编译器，运行器
+        //其中，下面的语句，动态代理，是在运行器的，因此，我们根本看不到的
+        Person proxy = (Person) Proxy.newProxyInstance(Person.class.getClassLoader(), new Class[]{Person.class}, 
+                new ProxyHandler(person));
+        
+        //说明，proxy是代理对象，代理person
+        // 或者你可以这么认为
+        // person是演员，而proxy是经纪人
+       
+        proxy.eat();
+        
+        //打印输出的是com.sun.proxy.$Proxy4
+        //可见，代理对象的类型，并不是Person,
+        //因为，代理对象也实现了Person的接口，因此，可以调用eat等方法
+        //代理对象，在内存中的名字是$Proxy4
+        //下面，创建一个方法，createProxyClassFile()来获取该字节数组
+        System.out.println("---->:\t"+  proxy.getClass().getName()); //com.sun.proxy.$Proxy4
 
+        //将运行时期的动态代理对象，保存到项目路径下面去
+        createProxyClassFile();
+        
+    }
     
+    private static void createProxyClassFile(){
+        byte[] bys= ProxyGenerator.generateProxyClass("$Proxy4", new Class[]{Person.class});
+        
+        //字节数组通过流的形式保存到项目文件中
+        try {
+            FileOutputStream fos = new FileOutputStream("$Proxy4.class");
+            
+            fos.write(bys);
+            
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     
 }
